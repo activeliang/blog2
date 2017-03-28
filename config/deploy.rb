b5ef1f0c9c4317593084a7d8fc06b8e1b8b65dec
activeliang <activeliang@gmail.com>
2017-03-28 15:50:03 +0800

    Add capistrano file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repo_url, "git@github.com:activeliang/blog2.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/apps/blog2'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -23,8 +24,12 @@ set :repo_url, "git@github.com:activeliang/blog2.git"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
